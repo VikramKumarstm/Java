a805063308a6291eb52a4a96d8a17798b19e9fa6
VikramKumarstm <vikramstm0000@gmail.com>
2024-08-08 09:21:07 +0530

    add initial files

---
+++ Nature.java
@@ -0,0 +1,5 @@
+public class Nature {
+    public static void main(String[] args) {
+        System.out.println("Hello Nature!");
+    }
+}
---
+++ This.java
@@ -0,0 +1,5 @@
+public class This {
+    public static void main(String[] args) {
+        System.out.println("This is this file");
+    }
+}
